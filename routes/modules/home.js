e01ce386f74783c5fc6b756c3d7fdb8d7e3fb8b1
huntershih <huntershih.tw@gmail.com>
2021-10-12 23:46:54 +0800

    modify ui all pages

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,22 +6,7 @@ const routers = express.Router()
 
 
 
-routers.get('/:short', (req, res) => {
-  const short = req.params.short
-  UrlShorteners.find({ short })
-    .lean()
-    .then(data => {
-      //如果短網址不存在
-      if (data.length === 0) {
-        //顯示"the link is not available"       
-        res.render('na', { short })
-      } else {
-        //如果短網址存在 - 導向到資料庫內儲存的網址
-        res.redirect(`${data.origin}`)
-      }
-    })
-    .catch(error => console.log(error))
-})
+
 
 
 routers.get('/', (req, res) => {
@@ -32,8 +17,9 @@ routers.post('/', (req, res) => {
   const origin = req.body.url
   let newShort = shortUrlGenerator(5)
 
+  //防止使用者輸入非網址或空白資訊並提醒
   if (!validUrl.isUri(origin))
-    res.status(418).send("InvalidURL")
+    res.status(418).send("Invalid Input Data")
 
   //確認網址型態
   UrlShorteners.find({ origin })
@@ -55,9 +41,23 @@ routers.post('/', (req, res) => {
       }
     })
     .catch(err => console.log(err))
-
-
 })
 
+routers.get('/:short', (req, res) => {
+  const short = req.params.short
+  UrlShorteners.find({ short })
+    .lean()
+    .then(data => {
+      //如果短網址不存在
+      if (data.length === 0) {
+        //顯示"the link is not available"       
+        res.render('na', { short })
+      } else {
+        //如果短網址存在 - 導向到資料庫內儲存的網址
+        res.redirect(`${data[0].origin}`)
+      }
+    })
+    .catch(error => console.log(error))
+})
 
 module.exports = routers
